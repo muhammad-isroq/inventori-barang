bafb7f4ff89937fc958572456b7c0b0231f9c1a6
muhammad-isroq <muhammadisroq7@gmail.com>
2024-10-26 18:18:55 +0700

    update view login

--- application/views/V_login.php
+++ application/views/V_login.php
@@ -32,36 +32,45 @@
     }
 </style>
 <body class="text-center bg-secondary-subtle">
-    <div class="bg-light fw-bold rounded fs-6" id="sistem">
-        <img class="m-3" src="<?=base_url();?>/assets/logo-sistem.png" alt="" width="50" height="50">Sistem Inventori Barang
+    <div class="container-fluid mx auto">
+        <div class="row row-md-2">
+            <!-- <div class="col-md mb-3">
+                <img src="<?= base_url();?>/assets/penyimpanan.jpg" class="img-fluid rounded" alt="">
+            </div> -->
+            <div class="col-md shadow-md">
+                <div class="bg-light fw-bold rounded fs-6 shadow bg-body-tertiary rounded" id="sistem">
+                    <img class="m-3" src="<?=base_url();?>/assets/logo-sistem.png" alt="" width="50" height="50">Sistem Inventori Barang
+                </div>
+                <br>
+                <form class="form-signin bg-body rounded form-lg shadow bg-body-tertiary rounded" method="POST" action="<?= base_url('Auth/login');?>"
+                    >
+                    <img class="mb-4" src="<?=base_url();?>/assets/logo-login.png" alt="" width="100" height="100">
+                    <h1 class="h3 mb-3 font-weight-normal">Log In</h1>    
+
+                    <label for="inputEmail" class="sr-only float-start mb-2">Username</label>
+                    <input type="text" class="form-control mb-3" name="username" placeholder="Username" id="usr" required autofocus>
+                    <label for="inputPassword" class="sr-only float-start mb-2">Password</label>
+                    <input type="password" id="inputPassword" class="form-control" name="password" placeholder="Password" required>
+                    <button class="btn btn-primary btn-block btn-sm form-control mt-3" type="submit">Login</button>
+                </form>
+            </div>
+        </div>
     </div>
-    <br>
-    <form class="form-signin bg-body rounded form-lg" method="POST" action="<?= base_url('Auth/login');?>"
-    >
-    <img class="mb-4" src="<?=base_url();?>/assets/logo-login.png" alt="" width="100" height="100">
-    <h1 class="h3 mb-3 font-weight-normal">Log In</h1>    
 
-    <label for="inputEmail" class="sr-only float-start mb-2">Username</label>
-    <input type="text" class="form-control mb-3" name="username" placeholder="Username" id="usr" required autofocus>
-    <label for="inputPassword" class="sr-only float-start mb-2">Password</label>
-    <input type="password" id="inputPassword" class="form-control" name="password" placeholder="Password" required>
-    <button class="btn btn-dark btn-block btn-sm form-control mt-3" type="submit">Login</button>
-</form>
+    <script type="text/javascript">
+        const usr = document.getElementById('usr');
+        usr.addEventListener("keydown", function(){
+            usr.classList.add('bg-success-subtle');
 
-<script type="text/javascript">
-    const usr = document.getElementById('usr');
-    usr.addEventListener("keydown", function(){
-        usr.classList.add('bg-success-subtle');
-        
-    })
+        })
 
-    const pss = document.getElementById('inputPassword');
-    pss.addEventListener("keydown", function(){
-        pss.classList.add('bg-success-subtle');
-        
-    })
-</script>
+        const pss = document.getElementById('inputPassword');
+        pss.addEventListener("keydown", function(){
+            pss.classList.add('bg-success-subtle');
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        })
+    </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
